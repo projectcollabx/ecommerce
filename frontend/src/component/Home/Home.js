e30ae4fb4e3825280c7bfd416a248bb5c930b7ec
projectcollabx <projectcollabx@gmail.com>
2023-06-08 16:53:48 +0530

    first commit

---
+++ frontend/src/component/Home/Home.js
@@ -0,0 +1,86 @@
+import React, { Fragment, useEffect, useState } from "react";
+import { CgMouse } from "react-icons/cg";
+import "./Home.css";
+import ProductCard from "./ProductCard.js";
+import MetaData from "../layout/MetaData";
+import { clearErrors, getProduct } from "../../actions/productAction";
+import { useSelector, useDispatch } from "react-redux";
+import Loader from "../layout/Loader/Loader";
+import { useAlert } from "react-alert";
+import CountUp from 'react-countup';
+import ScrollTriger from 'react-scroll-trigger';
+import './Counter.css';
+const Home = () => {
+  const alert = useAlert();
+  const dispatch = useDispatch();
+  const { loading, error, products } = useSelector((state) => state.products);
+
+  useEffect(() => {
+    if (error) {
+      alert.error(error);
+      dispatch(clearErrors());
+    }
+    dispatch(getProduct());
+  }, [dispatch, error, alert]);
+
+  const [counterOn,setCounterOn]=useState(false);
+  return (
+    <Fragment>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Fragment>
+          <MetaData title="ECOMMERCE" />
+
+          <div className="banner">
+            <p>Welcome to Ecommerce</p>
+            <h1>FIND AMAZING PRODUCTS BELOW</h1>
+
+            <a href="#container">
+              <button>
+                Scroll <CgMouse />
+              </button>
+            </a>
+          </div>
+          <ScrollTriger onEnter={()=>setCounterOn(true)} onExit={()=>setCounterOn(false)}>
+          <div className="counter-container">
+      <div className="counter counter-animation">
+            {counterOn && <CountUp start={0} end={1000} duration={5} delay={0} useEasing={true}
+          useGrouping={true}/>}
+            +
+            </div>
+            <div className="counter counter-animation">
+            {counterOn && <CountUp start={0} end={4} duration={15} delay={0} useEasing={true}
+          useGrouping={true}/>}
+            +
+            </div>
+            <div className="counter counter-animation">
+            {counterOn && <CountUp start={0} end={300} duration={5} delay={0} useEasing={true}
+          useGrouping={true}/>}
+            +
+            </div>
+            <div className="counter counter-animation">
+            {counterOn && <CountUp start={0} end={700} duration={5} delay={0} useEasing={true}
+          useGrouping={true}/>}
+            +
+            </div>
+    </div>
+          </ScrollTriger>
+          
+          <h2 className="homeHeading">Featured Products</h2>
+          {/* <CountUp end = {100} duration = {5}/> */}
+          <div className="container" id="container">
+            {products &&
+              products.map((product) => (
+                <ProductCard key={product._id} product={product} />
+              ))}
+          </div>
+          
+          {/* <Temp max_value={100} duration={50}></Temp> */}
+        </Fragment>
+      )}
+    </Fragment>
+  );
+};
+
+export default Home;
---
+++ frontend/src/component/layout/Header/Header.jsx
@@ -0,0 +1,100 @@
+// import React from "react";
+// import { ReactNavbar } from "overlay-navbar";
+// import logo from "../../../images/logo.png";
+
+// const options = {
+//   burgerColorHover: "#eb4034",
+//   logo,
+//   logoWidth: "20vmax",
+//   navColor1: "white",
+//   logoHoverSize: "10px",
+//   logoHoverColor: "#eb4034",
+//   link1Text: "Home",
+//   link2Text: "Products",
+//   link3Text: "Contact",
+//   link4Text: "About",
+//   link1Url: "/",
+//   link2Url: "/products",
+//   link3Url: "/contact",
+//   link4Url: "/about",
+//   link1Size: "1.3vmax",
+//   link1Color: "rgba(35, 35, 35,0.8)",
+//   nav1justifyContent: "flex-end",
+//   nav2justifyContent: "flex-end",
+//   nav3justifyContent: "flex-start",
+//   nav4justifyContent: "flex-start",
+//   link1ColorHover: "#eb4034",
+//   link1Margin: "1vmax",
+//   profileIconUrl: "/login",
+//   profileIconColor: "rgba(35, 35, 35,0.8)",
+//   searchIconColor: "rgba(35, 35, 35,0.8)",
+//   cartIconColor: "rgba(35, 35, 35,0.8)",
+//   profileIconColorHover: "#eb4034",
+//   searchIconColorHover: "#eb4034",
+//   cartIconColorHover: "#eb4034",
+//   cartIconMargin: "1vmax",
+// };
+
+// const Header = () => {
+//   return <ReactNavbar {...options} />;
+// };
+
+// export default Header;
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-black bg-black">
+      <div className="container-fluid">
+        {/* <Link className = "navbar-brand" to = "/">Navbar</Link> */}
+        <button
+          className="navbar-toggler bg-light"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className="nav-link active"
+                aria-current="page"
+                to="/Product"
+              >
+                Products
+              </Link>
+            </li>
+            <li className="nav-item dropdown">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/Contact"
+                >
+                  Contact
+                </Link>
+              </li>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" aria-current="page" to="/About">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
